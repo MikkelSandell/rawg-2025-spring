add599efb9e9dc773054f66a5f445e37a4f1ce7e
Mikkel sandell <mikkelsandell@gmail.com>
2025-02-17 12:34:32 +0100

    added genre lsit

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -9,14 +10,14 @@ function App() {
         base: `"header" "main"`,
         lg: `"header header" "aside main"`,
       }}
-      templateColumns={{base: "1fr", lg: "200px 1fr"}}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
       <GridItem pl="2" area={"header"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" bg="pink.300" area={"aside"}>
-          aside
+        <GridItem pl="2" area={"aside"}>
+          <GenreList/>
         </GridItem>
       </Show>
       <GridItem pl="2" area={"main"}>
