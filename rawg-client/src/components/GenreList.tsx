e2a6f71b3126c1a4bce86568168684b688162f02
Mikkel sandell <mikkelsandell@gmail.com>
2025-02-24 10:59:53 +0100

    implemented filtering games by genre

--- rawg-client/src/components/GenreList.tsx
+++ rawg-client/src/components/GenreList.tsx
@@ -1,9 +1,18 @@
-import { Heading, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, Heading, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props{
+  onSelectedGenre: (genre: Genre) => void;
+  selectGenre: Genre | null
+  
+}
+
+const GenreList = ({onSelectedGenre, selectGenre}: Props) => {
   const { data: genres, isLoading, error } = useGenres();
+
+  const getColor= (genre: Genre) =>
+    selectGenre?.id === genre.id ? "yellow" :"gray"
 
   if(error) return null;
 
@@ -23,7 +32,14 @@ const GenreList = () => {
                 borderRadius={8}
                 objectFit="cover"
               />
+              <Button
+              colorScheme={getColor(genre)}
+              variant="link"
+              fontSize="lg"
+              onClick={() => onSelectedGenre(genre)}>
+              
               <Text>{genre.name}</Text>
+              </Button>
             </HStack>
           </ListItem>
         ))}
