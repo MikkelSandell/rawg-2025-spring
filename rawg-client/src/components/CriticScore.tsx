967d203df787362bda04e47ee9de2b712e71d67a
Mikkel sandell <mikkelsandell@gmail.com>
2025-03-02 15:16:16 +0100

    added store

--- rawg-client/src/components/CriticScore.tsx
+++ rawg-client/src/components/CriticScore.tsx
@@ -1,27 +1,29 @@
+import { Badge } from "@chakra-ui/react";
 
-import { Badge } from '@chakra-ui/react';
-
-interface Props{
-    score: number;
+interface Props {
+  score: number;
 }
 
-const CriticScore = ({ score}: Props) =>{
-    const getColor = (score: number) =>{
-        if (score >= 75) return "green";
-        if (score >= 50) return "yellow";
-        return "red"
-    }
+const CriticScore = ({ score }: Props) => {
+  const getColor = (score: number) => {
+    if (score >= 75) return "green";
+    if (score >= 50) return "yellow";
+    return "red";
+  };
 
-    if (!score) return null;
+  if (!score) return null;
 
-    return <Badge 
-    border="1px solid" 
-    fontSize="14px" 
-    paddingX={2} 
-    borderRadius="4px" 
-    colorScheme={getColor(score)}>
-        {score}
+  return (
+    <Badge
+      border="1px solid"
+      fontSize="14px"
+      paddingX={2}
+      borderRadius="4px"
+      colorScheme={getColor(score)}
+    >
+      {score}
     </Badge>
-}
+  );
+};
 
-export default CriticScore
+export default CriticScore;
--- rawg-client/src/components/NavBar.tsx
+++ rawg-client/src/components/NavBar.tsx
@@ -1,15 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
-import ColderModeSwirch from "./ColorModeSwitch";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
-      <ColderModeSwirch></ColderModeSwirch>
+      <ColorModeSwitch />
     </HStack>
   );
 };
 
 export default NavBar;
-
