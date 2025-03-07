f1cdf5cdf2508678809235e83ee8cd08d9150ddd
Mikkel sandell <mikkelsandell@gmail.com>
2025-03-07 11:20:39 +0100

    made so i can use local database

--- rawg-server/startup/setupRouters.ts
+++ rawg-server/startup/setupRouters.ts
@@ -1,8 +1,15 @@
 import express from "express";
-import genreRouter from "../routes/genreRouter"
+import genreRouter from "../routes/genreRouter";
+import storeRouter from "../routes/StoreRouter";
+import parentPlatformRouter from "../routes/parentPlatformRouter";
+import gameRouter from "../routes/GameRouter";
 
+//assing endpoints to the routers
 const setupRouters = (app: express.Application) => {
-    app.use("/genre" , genreRouter);
-}
+  app.use("/genres", genreRouter);
+  app.use("/stores", storeRouter);
+  app.use("/platforms/lists/parents", parentPlatformRouter);
+  app.use("/games", gameRouter);
+};
 
 export default setupRouters;
