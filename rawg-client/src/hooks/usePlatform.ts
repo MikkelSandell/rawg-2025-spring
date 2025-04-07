43688dd5ecd5f88ae6e0f2ab6921dccac4ee725b
Mikkel sandell <mikkelsandell@gmail.com>
2025-04-07 11:35:41 +0200

    added use paltform

---
+++ rawg-client/src/hooks/usePlatform.ts
@@ -0,0 +1,12 @@
+import usePlatforms from "./usePlatforms";
+
+const usePlatfrom = (selectedPlatformId?: number) => {
+  const {data} = usePlatforms();
+  const selectedPlatform = data?.results?.find(
+    (platform) => platform.id === selectedPlatformId
+  );
+
+  return selectedPlatform
+}
+
+export default usePlatfrom;
